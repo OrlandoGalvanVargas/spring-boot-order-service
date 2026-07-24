f593411094ccaf27bc030b7aba7bfe3a8860f5a6
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-23 23:49:03 -0600

    feat: add new OrderRequest file

--- src/main/java/com/practica/order_service/dto/OrderRequest.java
+++ src/main/java/com/practica/order_service/dto/OrderRequest.java
@@ -1,4 +1,19 @@
 package com.practica.order_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class OrderRequest {
+
+    @NotBlank(message = "El nombre del cliente es obligatorio")
+    private String customerName;
+
+    @NotNull
+    @Positive(message = "El monto debe ser mayor a 0")
+    private BigDecimal totalAmount;
 }
