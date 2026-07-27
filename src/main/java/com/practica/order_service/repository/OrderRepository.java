64da705c1a4685e0bb627a4c0c9225c70ced9241
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-26 23:47:47 -0600

    feat: add comment to OrderRepository

--- src/main/java/com/practica/order_service/repository/OrderRepository.java
+++ src/main/java/com/practica/order_service/repository/OrderRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+// file Repository to OrderService
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
