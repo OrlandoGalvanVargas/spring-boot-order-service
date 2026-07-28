074306cad57ba019f4afa19626e2e6e4ac4f7a72
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-27 23:51:08 -0600

    feat: add comment to GlobalExceptionHandler

--- src/main/java/com/practica/order_service/exception/OrderNotFoundException.java
+++ src/main/java/com/practica/order_service/exception/OrderNotFoundException.java
@@ -1,5 +1,6 @@
 package com.practica.order_service.exception;
 
+// File exception to Order
 public class OrderNotFoundException extends RuntimeException{
     public OrderNotFoundException(Long id){
         super("No se encontró la orden con el id: " + id);
