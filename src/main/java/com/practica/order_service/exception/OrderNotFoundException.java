e626a0d2fad85279cf9ce60dc88eea6f63c1a879
Orlando G.V <orlando.gal.var.05@gmail.com>
2026-07-27 23:51:32 -0600

    feat: add comment to OrderNotFoundException

--- src/main/java/com/practica/order_service/exception/OrderNotFoundException.java
+++ src/main/java/com/practica/order_service/exception/OrderNotFoundException.java
@@ -1,6 +1,6 @@
 package com.practica.order_service.exception;
 
-// File exception to Order
+// File exception to OrderService
 public class OrderNotFoundException extends RuntimeException{
     public OrderNotFoundException(Long id){
         super("No se encontró la orden con el id: " + id);
